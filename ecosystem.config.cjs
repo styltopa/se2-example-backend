6eacc0bd82065b7f722b32a08f23dec1b7a77220
Stelios Topalidis <49813691+styltopa@users.noreply.github.com>
2022-11-02 13:37:05 +0200

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,11 @@
 module.exports = {
 	apps: [
 		{
-			name: "example-backend-0",
+			name: "example-backend-18",
 			script: "npm",
 			args: "start",
 			env: {
-				PORT: 3000,
+				PORT: 3018,
 			},
 		},
 	],
